19e1650fc353061ed8b7cd946f6fec43bd5a2715
anisul alam <anisulalam2002@gmail.com>
2026-04-08 14:39:35 +0600

    Implement realtime post broadcast using Django signals and Channels
    - Added signals.py to broadcast new posts on post_save
    - Send WebSocket group message to 'feed' channel layer
    - Implemented new_post handler in FeedConsumer
    - Updated frontend to render incoming posts via WebSocket
    - Added UI for new tweet card rendering

--- feed/apps.py
+++ feed/apps.py
@@ -2,4 +2,8 @@ from django.apps import AppConfig
 
 
 class FeedConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'feed'
+
+    def ready(self):
+        import feed.signals
--- feed/consumers.py
+++ feed/consumers.py
@@ -8,3 +8,12 @@ class FeedConsumer(AsyncWebsocketConsumer):
     
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard('feed', self.channel_name)
+    
+    async def new_post(self, event):
+        await self.send(text_data=json.dumps({
+            'content' : event['content'],
+            # 'username' : event['username', 'User'],
+            'username': event.get('username', 'User'),
+            'post_type' : event.get("post_type", "normal"),
+            "rank" : event.get("rank")
+        }))
